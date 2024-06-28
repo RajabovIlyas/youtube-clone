402a5c7148fe3a6db2259f4df306015e5994e776
RajabovIlyas <ilyasrajabow@gmail.com>
2024-06-28 11:28:46 +0500

    reduced search page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
-import { getVideoUrl } from "@/constants/endpoints-api.constant";
+import { getSearchUrl, getVideoUrl } from "@/constants/endpoints-api.constant";
 import { ResVideosModel } from "@/models/video.model";
 import { fetcher } from "@/services/fetcher.server";
 import { videoConverter } from "@/converters/video.converter";
 import VideoCard from "@/components/VideoCard";
 import { VideoCardModel } from "@/models/video-card.model";
+import { ResSearchModel } from "@/models/search.model";
+import { searchVideoConverter } from "@/converters/search-video.converter";
+
+interface HomeProps {
+  searchParams: { search_query: string };
+}
+
+const getVideosBySearchQuery = async (
+  search_query: string,
+  nextPageToken = "",
+): Promise<VideoCardModel[] | undefined> => {
+  try {
+    const res = await fetcher<ResSearchModel>(
+      getSearchUrl(search_query, nextPageToken),
+    );
+
+    return Promise.all(res.items.map(searchVideoConverter));
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log("error", error.message);
+    }
+  }
+};
 
 const getVideos = async (
   nextPageToken = "",
@@ -21,8 +44,12 @@ const getVideos = async (
   }
 };
 
-export default async function Home() {
-  const videos = await getVideos();
+export default async function Home({
+  searchParams: { search_query },
+}: HomeProps) {
+  const videos = await (search_query
+    ? getVideosBySearchQuery(search_query)
+    : getVideos());
 
   return (
     <main className="pt-5 mx-auto px-4 max-w-screen-2xl">
--- src/components/Header/SearchForm.tsx
+++ src/components/Header/SearchForm.tsx
@@ -4,28 +4,39 @@ import SearchIcon from "@/components/icons/SearchIcon";
 import { useSearchParams } from "next/navigation";
 import { FC, Suspense } from "react";
 
-interface SearchFormSuspenseProps {
+interface SearchFormProps {
   className?: string;
 }
 
-interface SearchFormProps {
+interface SearchInputProps {
   searchQuery?: string | null;
 }
 
-const SearchForm: FC<SearchFormSuspenseProps & SearchFormProps> = ({
-  className,
-  searchQuery,
-}) => {
+const SearchInput: FC<SearchInputProps> = ({ searchQuery }) => {
+  return (
+    <input
+      className="h-10 w-full bg-zinc-900 rounded-l-full focus-visible:border-sky-700 focus-visible:outline-0 pl-5 pr-2"
+      name="search_query"
+      defaultValue={searchQuery || ""}
+    />
+  );
+};
+
+const SearchInputWithParams = () => {
+  const searchParams = useSearchParams();
+  const searchQuery = searchParams.get("search_query");
+  return <SearchInput searchQuery={searchQuery} />;
+};
+
+const SearchForm: FC<SearchFormProps> = ({ className }) => {
   return (
     <form
-      action="/results"
+      action="/"
       className={"max-w-xl flex-grow justify-center items-center " + className}
     >
-      <input
-        className="h-10 w-full bg-zinc-900 rounded-l-full focus-visible:border-sky-700 focus-visible:outline-0 pl-5 pr-2"
-        name="search_query"
-        defaultValue={searchQuery || ""}
-      />
+      <Suspense fallback={<SearchInput />}>
+        <SearchInputWithParams />
+      </Suspense>
       <button
         type="submit"
         className="flex justify-center items-center px-3 bg-zinc-900 rounded-r-full w-16 h-10 hover:bg-zinc-800"
@@ -36,18 +47,4 @@ const SearchForm: FC<SearchFormSuspenseProps & SearchFormProps> = ({
   );
 };
 
-const SearchFormWithParams = <T,>(props: T) => {
-  const searchParams = useSearchParams();
-  const searchQuery = searchParams.get("search_query");
-  return <SearchForm {...props} searchQuery={searchQuery} />;
-};
-
-const SearchFormSuspense: FC<SearchFormSuspenseProps> = ({ className }) => {
-  return (
-    <Suspense fallback={<SearchForm className={className} />}>
-      <SearchFormWithParams className={className} />
-    </Suspense>
-  );
-};
-
-export default SearchFormSuspense;
+export default SearchForm;
